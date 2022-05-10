1972890874ec1a812fe1d93edf5f596e5ae87fa2
rathesh77 <rathesh.charles@outlook.fr>
2022-05-10 21:11:59 +0200

    update

--- Trie.py
+++ Trie.py
@@ -1,4 +1,10 @@
 from Node import *
+# begin et end inclus
+def slice(str, begin, end):
+    newstr = ''
+    for i in range (begin, end +1):
+        newstr += str[i]
+    return newstr
 
 class Trie:
     def __init__(self):
@@ -9,7 +15,7 @@ class Trie:
         i = 0
         ptr = self
         while i < len(word):
-            print(i, len(word))
+            #print(i, len(word))
             if word[i] in ptr.root.children:
                 ptr = ptr.root.children[word[i]]
                 i += 1
@@ -18,12 +24,12 @@ class Trie:
             elif len(ptr.root.children.keys()) == 0:
                 return None
             else:
-                print('toto')
+                ##print('toto')
                 for key in ptr.root.children:
                     w = ptr.root.children[key].search(word)
-                    print(w, key)
+                    #print(w, key)
                     if w != None:
-                        print(key)
+                        #print(key)
                         return w
                 break
         
@@ -32,34 +38,24 @@ class Trie:
     
     def insert(self, word):
         #TODO
-        i = 0
+        i = len(word)-1
         ptr = self
-        while (True):
-            found = ptr.search(word[i])
-            print('wesh')
-            if found != -1:
-                ptr = found
-                i += 1
-            else:
+       
+        while (i >= 0):
+            look_up = ptr.search(slice(word, 0, i))
+            if look_up != None:
+                ptr = look_up
                 break
-
-        return ptr
-        ptr = self.root
+            i-= 1
+        #return None
+        i = i +1 
         while i < len(word):
-            if word[i] in ptr.children:
-                ptr = ptr.children[word[i]]
-                i+=1
-            else:
-                #ptr.children[word[i]] = Node()
-                #i += 1
-                break
+            ptr.root.children[word[i]] = Trie()
+            ptr = ptr.root.children[word[i]]
+            i+=1
         
-        while i < len(word):
-            ptr.children[word[i]] =  Node()
-            ptr = ptr.children[word[i]]
-            i += 1
-
-        return self
+  
+        return ptr
         
     def collectAllWords(self, node=None, word="", words=[]):
         #TODO
--- main.py
+++ main.py
@@ -7,7 +7,10 @@ T = Trie()
 #T.insert('salut les amis')
 T.root.children['a'] = Trie()
 T.root.children['a'].root.children['b'] = Trie()
-T.root.children['a'].root.children['b'].root.children['c'] = Trie()
+T.root.children['a'].root.children['c'] = Trie()
 
-res = T.search('bac')
+T.root.children['a'].root.children['c'].root.children['d'] = Trie()
+
+res = T.search('e')
+res2 = T.insert('arme')
 print('fini')
