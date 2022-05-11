0c1eef31ef811b532735257e14325f294f824479
rathesh77 <rathesh.charles@outlook.fr>
2022-05-11 19:36:35 +0200

    refactor search function

--- Trie.py
+++ Trie.py
@@ -7,31 +7,15 @@ class Trie:
     # fonction de recherche d'un mot dans l'arbre
     def search(self, word):
         i = 0
-        # on souhaite avoir un pointeur qui va pointer vers chaque Tri au fur et à mesure qu'on descend dans l'arbre
         ptr = self
-        # On veut verifier la presence de chaque lettre de notre mot, dans ce même ordre, dans l'arbre
-        # la variable "i" stock la lettre courante et sera incrementé à chaque iteration
-        while i < len(word):
-            # si la lettre courante fait partie des lettres "enfants" du tri courant
-            if word[i] in ptr.root.children:
-                # on deplace le pointeur sur le tri associé à la lettre courante
-                ptr = ptr.root.children[word[i]]
-                # on passe à la lettre prochaine
-                i += 1
-                # si on est arrivé à derniere lettre du mot, on retourne l'arbre courant
-                if i == len(word):
-                    return ptr
-            # sinon, s'il ne reste plus de lettres dans le tri courant, on retourne None (ou NULL) car on peut deduire que le mot
-            # n'est pas stocké dans l'arbre
-            elif len(ptr.root.children.keys()) == 0:
-                return None
-            # autrement, on repete le processus pour tous les noeuds enfants restants
-            else:
-                for key in ptr.root.children:
-                    w = ptr.root.children[key].search(word)
-                    if w != None:
-                        return w
-                break
+        # on parcours l'arbre en profondeur tant que chaque objet Trie 
+        # comporte le n-eme caractere de word
+        while i < len(word) and word[i] in ptr.root.children:
+            ptr = ptr.root.children[word[i]]
+            if i == len(word) - 1:
+                return ptr
+            i += 1
+                
         return None
     # fonction d'insertion d'un mot dans l'arbre
     def insert(self, word):
